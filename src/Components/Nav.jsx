aaf17917e96d4cd0f666bb6db2cafaf419cfa9d4
Aadhil-official <aadhilmhm.21@uom.lk>
2024-07-16 19:39:24 +0530

    Navigation bar is updated

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,7 +1,7 @@
-import { AppBar, Avatar, Grid, Toolbar, Typography, createTheme, responsiveFontSizes } from '@mui/material'
+import { AppBar, Avatar, Button, Grid, Toolbar, Typography, createTheme, responsiveFontSizes, useMediaQuery } from '@mui/material'
 import React from 'react'
 import '../Styles/Components/Nav.css'
-import { NavLink } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import { ThemeProvider } from '@emotion/react';
 
 function Nav() {
@@ -9,11 +9,30 @@ function Nav() {
   let theme = createTheme();
   theme = responsiveFontSizes(theme);
 
+  const isLargerThan700px = useMediaQuery('(min-width:700px)');
+
   return (
     <div>
       <ThemeProvider theme={theme}>
         {/* <Toolbar> */}
-        <AppBar className='makeblur' sx={{ backgroundColor: 'rgb(255, 255, 255,0.4)' }}>
+        <AppBar sx={{ backgroundColor: 'rgb(255, 255, 255,0.3)' }}>
+
+          {isLargerThan700px && <Grid container sx={{ marginBottom: '-27.5px' }}>
+            <Grid item xs={12} sx={{ marginRight: "20px" }} textAlign='right'>
+              <Button variant='outlined' sx={{ marginRight: "5px", padding: "2px 6px" }}>
+                <Typography variant='body2'>
+                  Sign in
+                </Typography>
+              </Button>
+
+              <Button variant='outlined' sx={{ padding: "2px 6px" }}>
+                <Typography variant='body2'>
+                  Sign up
+                </Typography>
+              </Button>
+            </Grid>
+          </Grid>}
+
           <Grid container justifyContent='center' className='nav'>
             {/* <Grid item xs={3}></Grid> */}
             {/* <Grid item xs={1}> */}
@@ -30,13 +49,33 @@ function Nav() {
               <NavLink to="/BrowseRooms" className='cont' >Contact</NavLink>
             </Typography>
             {/* <Grid item sm={1} md={2.5}></Grid> */}
-            <Grid item xs={8}></Grid>
-
+            <Grid item xs={8} textAlign='right'></Grid>
           </Grid>
           {/* <Grid item xs={3} sm={3} md={3} lg={3} xl={3}></Grid> */}
           {/* </Grid> */}
           {/* </Toolbar> */}
+
         </AppBar>
+
+        {!isLargerThan700px && <Grid container position='fixed' sx={{ marginBottom: '-70px', marginTop: '5px', zIndex: 100 }}>
+          <Grid item xs={12} sx={{ marginRight: "20px" }} textAlign='right'>
+            <br /><br /><Button variant='contained' sx={{ marginRight: "5px", padding: "2px 6px" }}>
+              <Typography variant='body2' sx={{ fontSize: '0.75rem !important' }}>
+                Sign in
+              </Typography>
+            </Button>
+
+            <Link to='/addHotel'>
+              <Button variant='contained' sx={{ padding: "2px 6px" }}>
+                <Typography variant='body2' sx={{ fontSize: '0.75rem !important' }}>
+                  Sign up
+                </Typography>
+              </Button>
+            </Link>
+
+          </Grid>
+        </Grid>}
+
       </ThemeProvider>
     </div>
   )
